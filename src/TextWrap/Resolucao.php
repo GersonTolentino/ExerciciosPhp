1f49fe7860a4bb4abd403d28ccf0e4bb01881af6
GersonTolentino <43996821+GersonTolentino@users.noreply.github.com>
2020-11-28 15:19:05 -0300

    Update Resolucao.php
    Desculpa realizar mais de uma vez aqui levou algum tempo para aprender a mexer no github

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -24,20 +24,19 @@ class Resolucao implements TextWrapInterface {
    * testes unitários.
    */
   public function textWrap(string $texto, int $limitador): array {
-    $texto = $texto." " ;//adicionando " "(espaço) pois minha condicional trabalha com ela, caso não colocar da erro nas ultimas duas linhas
-    while(strlen($texto)!=0){//enquano possuir string dentro da variavel texto o loop ocorre
-      $fragmento=substr(ltrim($texto),0,$limitador+1);
-      if(empty(strripos($fragmento," ")) == true){
-        $texto= ltrim(substr($texto,($limitador),strlen($texto)));
+    $texto = ($texto . " "); 
+    while (strlen($texto) != 0) { 
+      $fragmento = substr(ltrim($texto) ,0 ,$limitador + 1); 
+      // Adicionado "1" para pegar até o caractere demilimado, cso contrário não daria certo.
+      if (empty(strripos($fragmento, " ")) == TRUE) { 
+        $texto = ltrim(substr($texto, ($limitador), strlen($texto)));
       }
-      else{
-        $texto = ltrim(substr($texto,(strripos($fragmento," ")),strlen($texto)));
-        $fragmento = substr((trim($fragmento)),0,(strripos($fragmento," ")));
+      else {
+        $texto = ltrim(substr($texto, (strripos($fragmento, " ")), strlen($texto)));
+        $fragmento = substr((trim($fragmento)), 0, (strripos($fragmento, " ")));
       }
-      $arr[]=$fragmento;
+      $arr[] = $fragmento;
     }
     return ($arr);
-    }
- 
-
+  }
 }
