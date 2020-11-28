4bbe384effaff0cdac0afb78b301ed9fb82620c1
GersonTolentino <43996821+GersonTolentino@users.noreply.github.com>
2020-11-28 15:21:45 -0300

    Update Resolucao.php

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -26,13 +26,13 @@ class Resolucao implements TextWrapInterface {
   public function textWrap(string $texto, int $limitador): array {
     $texto = ($texto . " "); 
     while (strlen($texto) != 0) { 
-      $fragmento = substr(ltrim($texto) ,0 ,$limitador + 1); 
+      $fragmento = substr(ltrim($texto) , 0 , ($limitador + 1)); 
       // Adicionado "1" para pegar até o caractere demilimado, cso contrário não daria certo.
       if (empty(strripos($fragmento, " ")) == TRUE) { 
-        $texto = ltrim(substr($texto, ($limitador), strlen($texto)));
+        $texto = ltrim(substr($texto, ($limitador), (strlen($texto))));
       }
       else {
-        $texto = ltrim(substr($texto, (strripos($fragmento, " ")), strlen($texto)));
+        $texto = ltrim(substr($texto, (strripos($fragmento, " ")), (strlen($texto))));
         $fragmento = substr((trim($fragmento)), 0, (strripos($fragmento, " ")));
       }
       $arr[] = $fragmento;
